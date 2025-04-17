0eb4c647a690bc6e20c29be9a8690f434d9796a6
Arthur Mattos <arthur.mattos@softmake.com.br>
2025-04-17 10:52:47 -0300

    Instruções de instalação

---
+++ DatabaseScripts/Install.sql
@@ -0,0 +1,132 @@
+CREATE DATABASE [GrupoColorado]
+GO
+
+
+USE [GrupoColorado]
+GO
+
+CREATE TABLE [dbo].[Clientes](
+	[CodigoCliente] [int] IDENTITY(1,1) NOT NULL,
+	[RazaoSocial] [varchar](100) NOT NULL,
+	[NomeFantasia] [varchar](100) NOT NULL,
+	[TipoPessoa] [char](1) NOT NULL,
+	[Documento] [varchar](14) NOT NULL,
+	[Endereco] [varchar](100) NOT NULL,
+	[Complemento] [varchar](100) NULL,
+	[Bairro] [varchar](100) NOT NULL,
+	[Cidade] [varchar](100) NOT NULL,
+	[CEP] [char](8) NOT NULL,
+	[UF] [char](2) NOT NULL,
+	[DataInsercao] [datetime] NOT NULL,
+	[UsuarioInsercao] [int] NOT NULL,
+ CONSTRAINT [PK_Clientes] PRIMARY KEY CLUSTERED 
+(
+	[CodigoCliente] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[Telefones](
+	[CodigoCliente] [int] NOT NULL,
+	[NumeroTelefone] [varchar](11) NOT NULL,
+	[CodigoTipoTelefone] [int] NOT NULL,
+	[Operadora] [varchar](100) NOT NULL,
+	[Ativo] [bit] NOT NULL,
+	[DataInsercao] [datetime] NOT NULL,
+	[UsuarioInsercao] [int] NOT NULL,
+ CONSTRAINT [PK_Telefones] PRIMARY KEY CLUSTERED 
+(
+	[CodigoCliente] ASC,
+	[NumeroTelefone] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[TiposTelefone](
+	[CodigoTipoTelefone] [int] IDENTITY(1,1) NOT NULL,
+	[DescricaoTipoTelefone] [varchar](80) NOT NULL,
+	[DataInsercao] [datetime] NOT NULL,
+	[UsuarioInsercao] [int] NOT NULL,
+ CONSTRAINT [PK_TiposTelefone] PRIMARY KEY CLUSTERED 
+(
+	[CodigoTipoTelefone] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[Usuarios](
+	[CodigoUsuario] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](50) NOT NULL,
+	[Email] [varchar](250) NOT NULL,
+	[Senha] [varchar](50) NOT NULL,
+	[Ativo] [bit] NOT NULL,
+	[DataInsercao] [datetime] NOT NULL,
+ CONSTRAINT [PK_Usuarios] PRIMARY KEY CLUSTERED 
+(
+	[CodigoUsuario] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Clientes] ADD  CONSTRAINT [DF_Clientes_DataInsercao]  DEFAULT (getdate()) FOR [DataInsercao]
+GO
+
+ALTER TABLE [dbo].[Telefones] ADD  CONSTRAINT [DF_Telefones_Ativo]  DEFAULT ((1)) FOR [Ativo]
+GO
+
+ALTER TABLE [dbo].[Telefones] ADD  CONSTRAINT [DF_Telefones_DataInsercao]  DEFAULT (getdate()) FOR [DataInsercao]
+GO
+
+ALTER TABLE [dbo].[TiposTelefone] ADD  CONSTRAINT [DF_TiposTelefone_DataInsercao]  DEFAULT (getdate()) FOR [DataInsercao]
+GO
+
+ALTER TABLE [dbo].[Usuarios] ADD  CONSTRAINT [DF_Usuarios_Ativo]  DEFAULT ((1)) FOR [Ativo]
+GO
+
+ALTER TABLE [dbo].[Usuarios] ADD  CONSTRAINT [DF_Usuarios_DataInsercao]  DEFAULT (getdate()) FOR [DataInsercao]
+GO
+
+ALTER TABLE [dbo].[Clientes]  WITH CHECK ADD  CONSTRAINT [FK_Clientes_Usuarios] FOREIGN KEY([UsuarioInsercao])
+REFERENCES [dbo].[Usuarios] ([CodigoUsuario])
+GO
+
+ALTER TABLE [dbo].[Clientes] CHECK CONSTRAINT [FK_Clientes_Usuarios]
+GO
+
+ALTER TABLE [dbo].[Telefones]  WITH CHECK ADD  CONSTRAINT [FK_Telefones_Clientes] FOREIGN KEY([CodigoCliente])
+REFERENCES [dbo].[Clientes] ([CodigoCliente])
+GO
+
+ALTER TABLE [dbo].[Telefones] CHECK CONSTRAINT [FK_Telefones_Clientes]
+GO
+
+ALTER TABLE [dbo].[Telefones]  WITH CHECK ADD  CONSTRAINT [FK_Telefones_TiposTelefone] FOREIGN KEY([CodigoTipoTelefone])
+REFERENCES [dbo].[TiposTelefone] ([CodigoTipoTelefone])
+GO
+
+ALTER TABLE [dbo].[Telefones] CHECK CONSTRAINT [FK_Telefones_TiposTelefone]
+GO
+
+ALTER TABLE [dbo].[Telefones]  WITH CHECK ADD  CONSTRAINT [FK_Telefones_Usuarios] FOREIGN KEY([UsuarioInsercao])
+REFERENCES [dbo].[Usuarios] ([CodigoUsuario])
+GO
+
+ALTER TABLE [dbo].[Telefones] CHECK CONSTRAINT [FK_Telefones_Usuarios]
+GO
+
+ALTER TABLE [dbo].[TiposTelefone]  WITH CHECK ADD  CONSTRAINT [FK_TiposTelefone_TiposTelefone] FOREIGN KEY([CodigoTipoTelefone])
+REFERENCES [dbo].[TiposTelefone] ([CodigoTipoTelefone])
+GO
+
+ALTER TABLE [dbo].[TiposTelefone] CHECK CONSTRAINT [FK_TiposTelefone_TiposTelefone]
+GO
+
+ALTER TABLE [dbo].[TiposTelefone]  WITH CHECK ADD  CONSTRAINT [FK_TiposTelefone_Usuarios] FOREIGN KEY([UsuarioInsercao])
+REFERENCES [dbo].[Usuarios] ([CodigoUsuario])
+GO
+
+ALTER TABLE [dbo].[TiposTelefone] CHECK CONSTRAINT [FK_TiposTelefone_Usuarios]
+GO
+
+INSERT Usuarios (Nome, Email, Senha) VALUES ('Administrador', 'admin@grupocolorado.com.br', 'entrada#CRUD')
+GO
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # GrupoColorado
+
+Composição da solução:<br />
+
+<b>Database</b>: SQL Server Express 2022<br />
+<b>Backend</b>: ASP.NET WebAPI com .NET 6.0 e C#<br />
+<b>Frontend</b>: ASP.NET WebApp MVC com .NET 6.0, C# e jQuery<br />
+
+
+## Procedimentos para instalação
+
+1. Clone o repositório:<br />
+  <b>git clone https://github.com/arsmmd/GrupoColorado.git</b><br />
+
+2. Navegue até a raiz do diretório clonado:<br />
+  <b>cd .\GrupoColorado\ </b><br />
+
+3. Navegue até o diretório DatabaseScripts:<br />
+  <b>cd .\DatabaseScripts\ </b><br />
+
+4. Execute o script <b>Install.sql</b> em uma instância do SQL Server;<br />
+
+5. Navegue até a raiz do diretório clonado:<br />
+  <b>cd .\GrupoColorado\ </b><br />
+
+6. Abra o arquivo da solução (GrupoColorado.sln) no Visual Studio;<br />
+
+7. Ajuste a string de conexão no appSettings.json do projeto backend.<br />
+
+8. Quando tudo estiver configurado, o projeto pode ser executado e a tela de Login será exibida. O usuário de entrada está sendo inserido no script da base de dados.<br />
